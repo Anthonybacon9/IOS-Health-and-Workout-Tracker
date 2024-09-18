a34b6ec6ec3b521917591997abed6096983b8f43
Anthony <anthonybacon916@gmail.com>
2024-09-18 22:07:20 +0100

    Initial Commit

---
+++ Health and Workout Tracker/Health_and_Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_and_Workout_TrackerApp.swift
+//  Health and Workout Tracker
+//
+//  Created by Anthony Bacon on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Health_and_Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Health and Workout TrackerTests/Health_and_Workout_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Health_and_Workout_TrackerTests.swift
+//  Health and Workout TrackerTests
+//
+//  Created by Anthony Bacon on 18/09/2024.
+//
+
+import Testing
+@testable import Health_and_Workout_Tracker
+
+struct Health_and_Workout_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
