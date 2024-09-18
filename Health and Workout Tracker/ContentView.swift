df49bc22224a0b3f440382861f1ac25a457df951
Anthony <anthonybacon916@gmail.com>
2024-09-18 22:17:48 +0100

    removed full stop.

--- Health and Workout Tracker/ContentView.swift
+++ Health and Workout Tracker/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Health and Workout Tracker.")
+            Text("Health and Workout Tracker")
         }
         .padding()
     }
