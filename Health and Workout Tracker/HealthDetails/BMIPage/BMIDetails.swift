99d262b8987d56ca95c678287776e8c56cd554b8
Anthony <anthonybacon916@gmail.com>
2024-09-23 22:42:26 +0100

    NOT SURE WHAT IVE DONE SINCE LAST COMMIT

---
+++ Health and Workout Tracker/HealthDetails/BMIPage/BMIDetails.swift
@@ -0,0 +1,18 @@
+//
+//  BMIDetails.swift
+//  Health and Workout Tracker
+//
+//  Created by Anthony Bacon on 20/09/2024.
+//
+
+import SwiftUI
+
+struct BMIDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BMIDetails()
+}
